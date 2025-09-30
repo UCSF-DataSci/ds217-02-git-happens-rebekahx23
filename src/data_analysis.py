8499594421a8ea28e1df4dc5c826709dbafc91e8
rebekahx23 <170786201+rebekahx23@users.noreply.github.com>
2025-09-29 19:55:21 -0700

    add the generated python scripts

---
+++ src/data_analysis.py
@@ -0,0 +1,23 @@
+def load_students(filename):
+    # TODO: Read CSV and return list of student data
+    return
+
+def calculate_average_grade(students): 
+    # TODO: Calculate and return average
+    return 
+
+def count_math_students(students):
+    # TODO: Count students in Math
+    return
+
+def generate_report():
+    # TODO: Create formatted report string
+    return
+
+def save_report(report, filename):
+    # TODO: Write report to file
+    return
+
+def main():
+    # TODO: Orchestrate the analysis
+    return
---
+++ src/data_analysis_functions.py
@@ -0,0 +1,23 @@
+def load_data(filename):
+    # TODO: Generic loader that checks file extension
+    return
+
+def load_csv(filename):
+    # TODO: Load CSV data (same technique as basic script)
+    return
+
+def analyze_data(students):
+    # TODO: Return dictionary with multiple statistics
+    return
+
+def analyze_grade_distribution(grades):
+    # TODO: Count grades by letter grade ranges
+    return
+
+def save_results(results, filename):
+    # TODO: Save detailed report
+    return
+
+def main():
+    # TODO: Orchestrate the analysis using all functions
+    return
